2b0a63095e9a977fe61e86b7616528959d044505
collabologic <yuki.matsuda@gmail.com>
2018-12-19 21:56:41 +0900

    orchestraの実装

--- data/sound.go
+++ data/sound.go
@@ -1,15 +1,33 @@
 package data
 
-// 音声再生の指示データ
-type Sound struct {
-	PlayType PlayType
+import "github.com/veandco/go-sdl2/mix"
+
+// 音声の再生指示
+type Conduct struct {
+	ID       SoundIdentifier
+	Repeat   int
+	FadeTime int
+	Volume   int
 }
+
+// 音声データ
+type Sound struct {
+	ID       SoundIdentifier
+	PlayType PlayType
+	Music    *mix.Music
+	Effect   *mix.Chunk
+}
+
+// サウンドIDの列挙型（値はApp側で設定する）
+type SoundIdentifier int32
 
 // 演奏方法の列挙型
 type PlayType int
 
 // PlayType型の値
 const (
-	BGM    int = iota // PlayType型のBGM
-	EFFECT            // PlayType型の効果音
+	BGM    PlayType = iota // PlayType型のBGM
+	EFFECT                 // PlayType型の効果音
 )
+
+//
